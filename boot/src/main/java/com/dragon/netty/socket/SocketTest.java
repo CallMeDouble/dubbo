0ab8e9be9ea206871e8dc5cf93a142fe1759b5a5
zhushuanglong <zhushuanglong@yudianbank.com>
2018-03-23 17:52:37 +0800

    a

--- boot/src/main/java/com/dragon/netty/socket/SocketTest.java
+++ boot/src/main/java/com/dragon/netty/socket/SocketTest.java
@@ -1,8 +1,6 @@
 package com.dragon.netty.socket;
 
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 
@@ -10,13 +8,21 @@ import java.net.Socket;
  * Created by zhushuanglong on 2018/3/21.
  */
 public class SocketTest {
+    private static byte[] bytes = new byte[1024];
     public static void main(String[] args) throws Exception{
-        Socket client = new Socket("127.0.0.1", 8008);
+        Socket client = new Socket("127.0.0.1", 8888);
         OutputStream outputStream = client.getOutputStream();
         DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
-        dataOutputStream.writeUTF("你妹");
-        InputStream inputStream = client.getInputStream();
-        DataInputStream dataInputStream = new DataInputStream(inputStream);
-        System.out.println(dataInputStream.readUTF());
+        dataOutputStream.writeBytes("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+//        InputStream inputStream = client.getInputStream();
+//        DataInputStream dataInputStream = new DataInputStream(inputStream);
+//        int index = 0;
+//        while(true){
+//            byte b = dataInputStream.readByte();
+//            bytes[index] = b;
+//            if(b == -1){
+//                break;
+//            }
+//        }
     }
 }
