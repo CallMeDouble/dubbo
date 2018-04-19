118938e13765276e911e1c80c3715f4806eef43a
zhushuanglong <623029947@qq.com>
2018-04-19 22:05:00 +0800

    clone

---
+++ boot/src/main/java/com/dragon/pattern/Prototype/CloneTest.java
@@ -0,0 +1,17 @@
+package com.dragon.pattern.Prototype;
+
+/**
+ * Created by double on 18-4-10.
+ */
+public class CloneTest {
+
+    public static void main(String[] args) {
+
+    }
+}
+class Student{
+    private static int age = 1;
+        private String name = "张三";
+    private String address =new String("上海");
+
+}
