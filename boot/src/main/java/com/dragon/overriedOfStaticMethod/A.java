010d329f10f81f99d68f59e446a4eb49af1b4f05
zhushuanglong <623029947@qq.com>
2018-05-06 18:59:34 +0800

    staticMethod

---
+++ boot/src/main/java/com/dragon/overriedOfStaticMethod/A.java
@@ -0,0 +1,12 @@
+package com.dragon.overriedOfStaticMethod;
+
+/**
+ * Created by double
+ */
+public class A {
+     /*
+     父类的静态方法不能被覆盖
+     static void a(){
+        System.out.println("A.a()");
+    }*/
+}
---
+++ boot/src/main/java/com/dragon/overriedOfStaticMethod/B.java
@@ -0,0 +1,15 @@
+package com.dragon.overriedOfStaticMethod;
+
+import sun.reflect.Reflection;
+
+/**
+ * Created by double
+ */
+public class B extends A {
+    /*
+    不能将父类的方法覆盖为静态方法
+    @Override
+    static void a() {
+        super.a();
+    }*/
+}
