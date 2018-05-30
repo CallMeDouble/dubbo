deb2242dfba1cef1cca21cf1dfe3bb198414d899
zhushuanglong <zhushuanglong@yudianbank.com>
2018-05-30 10:56:37 +0800

    rabbitmq

---
+++ rabbitmq/src/main/java/com/example/rabbitmq/tutorial/HelloWorld/Receiver.java
@@ -0,0 +1,46 @@
+package com.example.rabbitmq.tutorial.HelloWorld;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Created by dragon
+ */
+public class Receiver {
+
+    private static final String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        //消费者和生产者一样，新建连接，创建通道，声明队列
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("192.168.64.128");
+        connectionFactory.setPort(5672);
+        connectionFactory.setUsername("double");
+        connectionFactory.setPassword("double");
+        Connection connection = connectionFactory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println("waiting for message");
+
+        //一个提供回调接口的对象，当有消息被推送时，回调该接口
+        DefaultConsumer defaultConsumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
+                    byte[] body) throws IOException {
+                String message = new String(body, "utf-8");
+                System.out.println("delivery message:"+message);
+            }
+        };
+        //设置消费的队列，是否ack，回调对象
+        channel.basicConsume(QUEUE_NAME, true, defaultConsumer);
+
+
+    }
+}
